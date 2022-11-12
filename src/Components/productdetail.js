731b322b4f680f03762b86ebef37d3775b195910
Prashanth <udupaprashanthkrishna@gmail.com>
2022-11-12 23:03:46 +0530

    Changes to be committed: 	modified:   src/Components/productdetail.js 	new file:   src/Components/productdetails.css

--- src/Components/productdetail.js
+++ src/Components/productdetail.js
@@ -1,21 +1,26 @@
 import { useParams } from "react-router-dom";
 import Header from "./header";
+import "./productdetails.css";
 function ProductDetail(props) {
   let params = useParams();
   const id = params.id;
   // eslint-disable-next-line
   let product = props.products.filter((item) => item.id == id);
+  const AddToCartHandler = () => {
+    console.log(id);
+  };
   return (
     <>
       <Header />
 
-      <div>
+      <div className="detail">
         <img
           src={require(`./../assets/products/${product[0].src}`)}
           alt={product[0].name}
         />
         <h3>{product[0].shortDescription}</h3>
         <p>{product[0].longDescription}</p>
+        <button onClick={AddToCartHandler}>Add to Cart</button>
       </div>
     </>
   );
