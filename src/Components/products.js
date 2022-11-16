c8ceef3d107338957c538c020448050bdf2102af
Prashanth <udupaprashanthkrishna@gmail.com>
2022-11-16 07:55:49 +0530

    Changes to be committed: 	modified:   src/Components/products.js

--- src/Components/products.js
+++ src/Components/products.js
@@ -1,9 +1,9 @@
 import Card from "./card";
 
 function Products({ selected, products, TextToSearch }) {
-  if (selected.includes("home") || selected.includes("phone")) {
+  if (TextToSearch !== "") {
     return products.map((item) => {
-      if (selected.includes(item.type)) {
+      if (item.name.toLowerCase().includes(TextToSearch.toLowerCase())) {
         return (
           <Card
             id={item.id}
@@ -16,17 +16,33 @@ function Products({ selected, products, TextToSearch }) {
       }
     });
   } else {
-    return products.map((item) => {
-      return (
-        <Card
-          src={item.src}
-          id={item.id}
-          name={item.name}
-          key={item.id}
-          type={item.type}
-        />
-      );
-    });
+    if (selected.includes("home") || selected.includes("phone")) {
+      return products.map((item) => {
+        if (selected.includes(item.type)) {
+          return (
+            <Card
+              id={item.id}
+              src={item.src}
+              name={item.name}
+              key={item.id}
+              type={item.type}
+            />
+          );
+        }
+      });
+    } else {
+      return products.map((item) => {
+        return (
+          <Card
+            src={item.src}
+            id={item.id}
+            name={item.name}
+            key={item.id}
+            type={item.type}
+          />
+        );
+      });
+    }
   }
 }
 
